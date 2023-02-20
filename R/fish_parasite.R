7accf0d702776ba318632c54c884be297fce40e8
Daniel Noble <daniel.noble@anu.edu.au>
2023-02-20 16:21:23 +1100

    Added a function and some new models

--- R/fish_parasite.R
+++ R/fish_parasite.R
@@ -264,6 +264,7 @@ dat
                       data = dat, iter = 6000, warmup = 2000, chains = 4, cores = 4, 
                       save_pars = save_pars(), file = "./output/models/model1", file_refit = "on_change",
                       control = list(adapt_delta = 0.98))
+    model1 <- add_criterion(model1, c("loo", "waic"))
     
     # Look at the MCMC chains.
     plot(model1)
@@ -273,15 +274,36 @@ dat
 
 ### Model 2: without tank effect
 
-    boldness_2 <- bf(log_boldness ~ 1 + treatment + (-1 + treatment |q| ID_fish) + (1 | cage)) + gaussian()
-    activity_2 <- bf(log_activity ~ 1 + treatment + (-1 + treatment |q| ID_fish) + (1 | cage)) + gaussian()
-    explore_2 <- bf(exploration ~ 1 + treatment + (-1 + treatment |q| ID_fish) + (1 | cage)) + gaussian()
+    boldness_2 <- bf(log_boldness ~ 1 + treatment + (-1 + treatment |q| ID_fish) + (1 | cage),
+                     sigma ~ -1 + treatment) + gaussian()
+    activity_2 <- bf(log_activity ~ 1 + treatment + (-1 + treatment |q| ID_fish) + (1 | cage),
+                     sigma ~ -1 + treatment) + gaussian()
+     explore_2 <- bf(exploration ~ 1 + treatment + (-1 + treatment |q| ID_fish) + (1 | cage),
+                     sigma ~ -1 + treatment) + gaussian()
     
     model2 <- brms::brm(boldness_2 + activity_2 + explore_2 + set_rescor(TRUE), 
                         data = dat, iter = 6000, warmup = 2000, chains = 4, cores = 4, 
                         save_pars = save_pars(), file = "./output/models/model2", file_refit = "on_change",
                         control = list(adapt_delta = 0.98))
+    model2 <- add_criterion(model2, c("loo", "waic"))
     
+    # Calculate repeatability
+    post_sd <- as_draws_df(model2, variable = "^sd", regex = TRUE)
+    
+       post_sd_C <- post_sd[,grepl("C", colnames(post_sd))]
+       post_sd_E <- post_sd[,grepl("E", colnames(post_sd))]
+    post_sd_cage <- post_sd[,grepl("cage", colnames(post_sd))]
+    post_sd_sig <- as_draws_df(model2, variable = "^sigma", regex = TRUE)
+    
+    # Repeatbility for boldness across treatment
+    source("./R/func.R")
+      
+      R_boldness <- repeatability(post_sd_C, post_sd_E, post_sd_cage, post_sd_sig, trait = "logboldness")
+       R_explore <- repeatability(post_sd_C, post_sd_E, post_sd_cage, post_sd_sig, trait = "exploration")
+       R_activity <- repeatability(post_sd_C, post_sd_E, post_sd_cage, post_sd_sig, trait = "logactivity")
+      
+      R_contrast <- R_explore - R_boldness
+      
     # Look at the MCMC chains.
     plot(model2)
     
---
+++ R/func.R
@@ -0,0 +1,17 @@
+#' @title repeatability
+#' @description Will take the posterior distributions from a multi-response model and calculate the posterior distribution of repeatbility
+#' @param C These are the SD estimates for control group at the between fish ID
+#' @param C These are the SD estimates for experimental group at the between fish ID
+#' @param cage These are the SD estimates for between cage
+#' @param sigma These are the SD estimates for within individual / residual 
+#' @param trait A character string with the name of the trait in the dataset
+
+repeatability <- function(C, E, cage, sigma, trait){
+  
+  combine_trt <- rowMeans(cbind(C[,grepl(trait, colnames(C))]^2, E[,grepl(trait, colnames(E))]^2))
+        total <- combine_trt + cage[, grepl(trait, colnames(cage))]^2 + sigma[,grepl(trait, colnames(sigma))]^2
+  
+  R <- combine_trt / total
+  
+  return(R)                                              
+}
