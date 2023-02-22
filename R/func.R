92ee1a6d8ec42a73e0e6cc3da9fed4370fd7dc2a
MaryaneGradito <maryane.gradito@umontreal.ca>
2023-02-22 16:35:38 +1100

    update repeatability function
    Just notice an typing error

--- R/func.R
+++ R/func.R
@@ -1,7 +1,7 @@
 #' @title repeatability
 #' @description Will take the posterior distributions from a multi-response model and calculate the posterior distribution of repeatbility
 #' @param C These are the SD estimates for control group at the between fish ID
-#' @param C These are the SD estimates for experimental group at the between fish ID
+#' @param E These are the SD estimates for experimental group at the between fish ID
 #' @param cage These are the SD estimates for between cage
 #' @param sigma These are the SD estimates for within individual / residual 
 #' @param trait A character string with the name of the trait in the dataset
