1ed70475f6b78cc256bc97bcb15b16a54cc9e033
MaryaneGradito <maryane.gradito@umontreal.ca>
2023-02-22 16:35:55 +1100

    new function
    to calculate repeatability for each group

---
+++ R/reap_each.R
@@ -0,0 +1,18 @@
+#' @title repeatability for each group
+#' @description Will take the posterior distributions from a multi-response model and calculate the posterior distribution of repeatbility
+#' @param G These are the SD estimates for one group at the between fish ID
+#' @param cage These are the SD estimates for between cage
+#' @param sigma These are the SD estimates for within individual / residual 
+#' @param trait A character string with the name of the trait in the dataset
+#' 
+
+rep_each <- function(G, cage, sigma, trait){
+  
+  combine_trt <- (C[,grepl(trait, colnames(C))]^2)
+  total <- combine_trt + cage[, grepl(trait, colnames(cage))]^2 + sigma[,grepl(trait, colnames(sigma))]^2
+  
+  R_ea <- combine_trt / total
+  
+  return(R_ea)                                              
+}
+
