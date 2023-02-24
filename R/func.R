ff9128691ccdfbac45877f1f233989c9d8d70742
Daniel Noble <daniel.noble@anu.edu.au>
2023-02-24 16:01:56 +1100

    pmcmc

--- R/func.R
+++ R/func.R
@@ -15,3 +15,10 @@ repeatability <- function(C, E, cage, sigma, trait){
   
   return(R)                                              
 }
+
+ #' @title pMCMC Function
+ #' @param x The vector for the posterior distribution. Note that this will test the null hypothesis that the parameter of interest is significantly different from 0. 
+ #' @param null A numeric value decsribing what the null hypothesis should be
+pmcmc <- function(x, null = 0){
+  2*(1 - max(table(x<null) / length(x)))
+}
