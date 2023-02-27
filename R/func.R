d3fa599dfebca62779a652741648bf8e7a178466
Daniel Noble <daniel.noble@anu.edu.au>
2023-02-27 14:14:49 +1100

    Modified pmcmc to have a one-tail option

--- R/func.R
+++ R/func.R
@@ -40,7 +40,12 @@ rep_each <- function(ID, cage, sigma, trait){
  #' @title pMCMC Function
  #' @param x The vector for the posterior distribution. Note that this will test the null hypothesis that the parameter of interest is significantly different from 0. 
  #' @param null A numeric value decsribing what the null hypothesis should be
-pmcmc <- function(x, null = 0){
-  2*(1 - max(table(x<null) / length(x)))
+ #' @param twotail Whether to conduct a one-tailed hypothesis or a two-tailed hypotheses. Default = true indicating a two-tailed test will be done.
+pmcmc <- function(x, null = 0, twotail = TRUE){
+  if(twotail){
+    2*(1 - max(table(x<=null) / length(x)))
+  } else{
+    (1 - max(table(x<=null) / length(x)))
+  }
 }
 
