673a59c62eefe804cb946071c85873b3075b457b
Daniel Noble <daniel.noble@anu.edu.au>
2023-02-28 12:15:43 +1100

    Update to models and  results

--- R/fish_parasite.R
+++ R/fish_parasite.R
@@ -38,15 +38,25 @@ pacman::p_load(lme4, rstan, StanHeaders, jsonlite, rstantools, brms, Rcpp, dplyr
 
 ### Model 1: with tank effect
 
-  boldness_1 <- bf(log_boldness ~ 1 + treatment + tank1 + (-1 + treatment |q| ID_fish) + (1 | cage)) + gaussian()
-  activity_1 <- bf(log_activity ~ 1 + treatment + tank2 + (-1 + treatment |q| ID_fish) + (1 | cage)) + gaussian()
-   explore_1 <- bf(exploration ~ 1 + treatment + tank2 + (-1 + treatment |q| ID_fish) + (1 | cage)) + gaussian()
+  boldness_1 <- bf(log_boldness ~ 1 + treatment + tank1 + (-1 + treatment|q| ID_fish) + (1 | cage)) + gaussian()
+  activity_1 <- bf(log_activity ~ 1 + treatment + tank2 + (-1 + treatment|q| ID_fish) + (1 | cage)) + gaussian()
+   explore_1 <- bf(exploration ~ 1 + treatment + tank2 + (-1 + treatment|q| ID_fish) + (1 | cage)) + gaussian()
    
   model1 <- brms::brm(boldness_1 + activity_1 + explore_1 + set_rescor(TRUE), 
                       data = dat, iter = 6000, warmup = 2000, chains = 4, cores = 4, 
                       save_pars = save_pars(), file = "./output/models/model1", file_refit = "on_change",
                       control = list(adapt_delta = 0.98))
     
+
+ boldness_1.2 <- bf(log_boldness ~ 1 + treatment + (1|q| ID_fish) + (1 | cage)) + gaussian()
+  activity_1.2 <- bf(log_activity ~ 1 + treatment + (1|q| ID_fish) + (1 | cage)) + gaussian()
+   explore_1.2 <- bf(exploration ~ 1 + treatment + (1|q| ID_fish) + (1 | cage)) + gaussian()
+   
+  model1.2 <- brms::brm(boldness_1 + activity_1 + explore_1 + set_rescor(TRUE), 
+                      data = dat, iter = 6000, warmup = 2000, chains = 4, cores = 4, 
+                      save_pars = save_pars(), file = "./output/models/model1", file_refit = "on_change",
+                      control = list(adapt_delta = 0.98))
+
     # Compare models  
   model1 <- add_criterion(model1, c("loo", "waic"))
     
