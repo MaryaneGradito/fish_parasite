09f2bb42364ccf784fab049e5655a0374f0712f4
Daniel Noble <daniel.noble@anu.edu.au>
2023-03-09 12:56:51 +1100

    Removing uneeded code

--- R/model2.R
+++ R/model2.R
@@ -20,45 +20,32 @@
 
 ### Model 2: without tank effect
 
-boldness_2 <- bf(log_boldness ~ 1 + treatment + (-1 + treatment |q| ID_fish) + (1 | cage),
-                 sigma ~ -1 + treatment) + gaussian()
-activity_2 <- bf(log_activity ~ 1 + treatment + (-1 + treatment |q| ID_fish) + (1 | cage),
-                 sigma ~ -1 + treatment) + gaussian()
-explore_2 <- bf(exploration ~ 1 + treatment + (-1 + treatment |q| ID_fish) + (1 | cage),
-                sigma ~ -1 + treatment) + gaussian()
+  boldness_2 <- bf(log_boldness ~ 1 + treatment + (-1 + treatment |q| ID_fish) + (1 | cage),
+                  sigma ~ -1 + treatment) + gaussian()
+  activity_2 <- bf(log_activity ~ 1 + treatment + (-1 + treatment |q| ID_fish) + (1 | cage),
+                  sigma ~ -1 + treatment) + gaussian()
+  explore_2 <- bf(exploration ~ 1 + treatment + (-1 + treatment |q| ID_fish) + (1 | cage),
+                  sigma ~ -1 + treatment) + gaussian()
 
-model2 <- brms::brm(boldness_2 + activity_2 + explore_2 + set_rescor(TRUE), 
-                    data = all_data, iter = 6000, warmup = 2000, chains = 4, cores = 4, 
-                    save_pars = save_pars(), file = "./output/models/model2", file_refit = "on_change",
-                    control = list(adapt_delta = 0.98))
-
-#rerun doesn't work
-if(rerun){
   model2 <- brms::brm(boldness_2 + activity_2 + explore_2 + set_rescor(TRUE), 
                       data = all_data, iter = 6000, warmup = 2000, chains = 4, cores = 4, 
-                      save_pars = save_pars(),
+                      save_pars = save_pars(), file = "./output/models/model2", file_refit = "on_change",
                       control = list(adapt_delta = 0.98))
   
-  saveRDS(model2, file = "./output/models/model2.rds")
-  
-} else{
-  model2 <- readRDS(file = "./output/models/model2.rds")
-}
-
 # Compare models  
-model2 <- add_criterion(model2 , c("loo", "waic"))
+  model2 <- add_criterion(model2 , c("loo", "waic"))
 
-saveRDS(model2, file = "./output/models/model2.rds")
+  saveRDS(model2, file = "./output/models/model2.rds")
 
 # Compare model 1 and 2: should we keep tank effect ? 
 
-model1 <- readRDS(file = "./output/models/model1.rds")
-model2 <- readRDS(file = "./output/models/model2.rds")
+  model1 <- readRDS(file = "./output/models/model1.rds")
+  model2 <- readRDS(file = "./output/models/model2.rds")
 
-loo(model1, model2)
+  loo(model1, model2)
 
 # Look at the MCMC chains.
-plot(model2)
+  plot(model2)
 
 # Look at the model
 summary(model2)
