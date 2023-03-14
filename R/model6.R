c381938d4f9e2e2edc3bb771d69c8de597a9878d
MaryaneGradito <maryane.gradito@umontreal.ca>
2023-03-14 17:18:27 +1100

    Update model6.R
    tried to pivot data for body condition.. doesn't work

--- R/model6.R
+++ R/model6.R
@@ -12,12 +12,18 @@
 # Load.packages
   pacman::p_load(lme4, rstan, StanHeaders, jsonlite, rstantools, brms, Rcpp, dplyr, here, flextable, pander, cmdstanr)
 
+#pivot data
+
+   dat2<- all_data  %>% 
+    pivot_longer(cols=c('z_bc_1','z_bc_2',"z_bc_3","z_bc_4"),
+                      values_to='body_condition') 
+  
+  
 ### Model 6: body condition as a response variable
 
   boldness_6 <- bf(log_boldness ~ 1 + z_parasite_load + (1 | ID_fish) + (1 | cage)) + gaussian()
   activity_6 <- bf(log_activity ~ 1 + z_parasite_load + (1 | ID_fish) + (1 | cage)) + gaussian()
   explore_6 <- bf(exploration ~ 1 + z_parasite_load + (1 | ID_fish) + (1 | cage)) + gaussian()
-  #which body condition? mean of all ? 
   body_condition <- bf(z_body_condition ~ 1  + (1 | ID_fish) + (1 | cage)) + gaussian()
   
   model_6 <- brms::brm(boldness_6 + activity_6 + explore_6 + set_rescor(TRUE), 
