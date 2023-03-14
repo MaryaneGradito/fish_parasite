d08087dd632de7bb094c442912f1d375540e9143
MaryaneGradito <maryane.gradito@umontreal.ca>
2023-03-14 13:34:56 +1100

    Update model6.R
    dropped tank effect + we need to add body condition as a response variable

--- R/model6.R
+++ R/model6.R
@@ -14,15 +14,19 @@
 
 ### Model 6: body condition as a response variable
 
-body_condition <- bf(z_body_condition ~ 1  + (1 | ID_fish) + (1 | cage)) + gaussian()
-
-
-model_BC <- brms::brm(boldness_C + activity_C + explore_C, data = dat, iter = 6000, warmup = 2000, chains = 4, cores = 4, 
-                      save_pars = save_pars(), file = "./output/models/model_BC", file_refit = "on_change",
-                      control = list(adapt_delta = 0.98))
+  boldness_6 <- bf(log_boldness ~ 1 + z_parasite_load + (1 | ID_fish) + (1 | cage)) + gaussian()
+  activity_6 <- bf(log_activity ~ 1 + z_parasite_load + (1 | ID_fish) + (1 | cage)) + gaussian()
+  explore_6 <- bf(exploration ~ 1 + z_parasite_load + (1 | ID_fish) + (1 | cage)) + gaussian()
+  #which body condition? mean of all ? 
+  body_condition <- bf(z_body_condition ~ 1  + (1 | ID_fish) + (1 | cage)) + gaussian()
+  
+  model_6 <- brms::brm(boldness_6 + activity_6 + explore_6 + set_rescor(TRUE), 
+                       data = dat_E, iter = 6000, warmup = 2000, chains = 4, cores = 4, 
+                       save_pars = save_pars(), file = "./output/models/model_6", file_refit = "on_change",
+                       control = list(adapt_delta = 0.98))
 
 # Look at the MCMC chains.
-plot(model_BC)
+plot(model_6)
 
 # Look at the model
-summary(model_BC)
+summary(model_6)
