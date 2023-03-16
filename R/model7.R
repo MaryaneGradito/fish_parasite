d37ac595b6332de1dd670c5925312130d23dc8b9
MaryaneGradito <maryane.gradito@umontreal.ca>
2023-03-16 13:24:37 +1100

    Create model7.R
    new model to look at parasites

---
+++ R/model7.R
@@ -0,0 +1,31 @@
+### Script for MODEL 7
+
+#############################
+# Import processed data
+#############################
+dat_parasite <- read.table("./output/dat_models_parasite.csv",header=T, sep=",")
+
+#############################
+# Model 7: Looking at the effects of two parasites
+#############################
+
+# Load.packages
+pacman::p_load(lme4, rstan, tidyverse, StanHeaders, jsonlite, rstantools, brms, Rcpp, dplyr, here, flextable, pander)
+
+### Model 7
+
+boldness_7 <- bf(log_boldness ~ 1 + z_ces + z_bs + z_bc + (1 | ID_fish) + (1 | cage)) + gaussian()
+activity_7 <- bf(log_activity ~ 1 + z_ces + z_bs + z_bc + (1 | ID_fish) + (1 | cage)) + gaussian()
+explore_7 <- bf(exploration  ~ 1 + z_ces + z_bs + z_bc + (1 | ID_fish) + (1 | cage)) + gaussian()
+
+
+model_7 <- brms::brm(boldness_7 + activity_7 + explore_7 + set_rescor(TRUE), 
+                     data = dat_parasite, iter = 6000, warmup = 2000, chains = 4, cores = 4, 
+                     save_pars = save_pars(), file = "./output/models/model_7", file_refit = "on_change",
+                     control = list(adapt_delta = 0.98))
+
+# Look at the MCMC chains.
+plot(model_7)
+
+# Look at the model
+summary(model_7)
