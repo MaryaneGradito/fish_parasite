7811b3b92836a8746e69ad2a5a2d47600acc7da8
MaryaneGradito <maryane.gradito@umontreal.ca>
2023-03-17 15:23:29 +1100

    Create model4.1.R
    rename model 3 for model 4

---
+++ R/model4.1.R
@@ -0,0 +1,35 @@
+### Script for MODEL 4
+
+###This model is part of step 2 of the analysis plan
+###Subset the experimental and control fish into two datasets (60 fish and 4 measurements for each C and E group)
+### Will allow us to answer:
+###Question 3: How does behaviour change with parasite infection?
+
+
+#############################
+# Import processed data
+#############################
+dat_E <- read.table("./output/dat_models_E.csv",header=T, sep=",")
+
+#############################
+# Model 4: effect of parasite on experimental group with parasite load not linear
+#############################
+
+# Load.packages
+  pacman::p_load(lme4, rstan, StanHeaders, jsonlite, rstantools, brms, Rcpp, dplyr, here, flextable, pander, cmdstanr)
+
+### Model 4: experimental group
+
+  boldness_4 <- bf(log_boldness ~ 1 + z_pl + I(z_pl)^2 + (1 | ID_fish) + (1 | cage)) + gaussian()
+  activity_4 <- bf(log_activity ~ 1 + z_pl + I(z_pl)^2 + (1 | ID_fish) + (1 | cage)) + gaussian()
+   explore_4 <- bf(exploration  ~ 1 + z_pl + I(z_pl)^2 + (1 | ID_fish) + (1 | cage)) + gaussian()
+
+  model_4 <- brms::brm(boldness_4 + activity_4 + explore_4 + set_rescor(TRUE), data = dat_E, iter = 6000, warmup = 2000, chains = 4, cores = 4, 
+                      save_pars = save_pars(), file = "./output/models/model_4", file_refit = "on_change",
+                      control = list(adapt_delta = 0.98))
+
+# Look at the MCMC chains.
+plot(model_4)
+
+# Look at the model
+summary(model_4)
