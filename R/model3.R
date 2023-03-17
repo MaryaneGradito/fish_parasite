d8dbb88ab5a11d84e1956b393fcb2558669aee07
MaryaneGradito <maryane.gradito@umontreal.ca>
2023-03-17 15:23:14 +1100

    Update model3.R
    created model 3 for random slopes

--- R/model3.R
+++ R/model3.R
@@ -1,36 +1,32 @@
 ### Script for MODEL 3
 
-###This model is part of step 2 of the analysis plan
-###Subset the experimental and control fish into two datasets (60 fish and 4 measurements for each C and E group)
-### Will allow us to answer:
-###Question 3: How does behaviour change with parasite infection?
-
 #############################
 # Import processed data
 #############################
-dat_E <- read.table("./output/dat_models_E.csv",header=T, sep=",")
+all_data <- read.table("./output/all_data_p.csv",header=T, sep=",")
 
 #############################
-# Model 3: effect of parasite on experimental group
+# Model 3: random slopes
 #############################
 
 # Load.packages
-  pacman::p_load(lme4, rstan, StanHeaders, jsonlite, rstantools, brms, Rcpp, dplyr, here, flextable, pander, cmdstanr)
+pacman::p_load(lme4, rstan, StanHeaders, jsonlite, rstantools, brms, Rcpp, dplyr, here, flextable, pander, tidyr, cmdstanr)
 
-### Model 3: experimental group
+### Model 3:
 
-  boldness_3 <- bf(log_boldness ~ 1 + z_pl + (1 | ID_fish) + (1 | cage)) + gaussian()
-  activity_3 <- bf(log_activity ~ 1 + z_pl + (1 | ID_fish) + (1 | cage)) + gaussian()
-  explore_3 <- bf(exploration ~ 1 + z_pl  + (1 | ID_fish) + (1 | cage)) + gaussian()
+boldness_3 <- bf(log_boldness ~ 1 + treatment + (treatment |q| ID_fish) + (1 | cage),
+                 sigma ~ -1 + treatment) + gaussian()
+activity_3 <- bf(log_activity ~ 1 + treatment + (treatment |q| ID_fish) + (1 | cage),
+                 sigma ~ -1 + treatment) + gaussian()
+explore_3 <- bf(exploration ~ 1 + treatment + (treatment |q| ID_fish) + (1 | cage),
+                sigma ~ -1 + treatment) + gaussian()
 
-  model_3 <- brms::brm(boldness_3 + activity_3 + explore_3 + set_rescor(TRUE), 
-                      data = dat_E, iter = 6000, warmup = 2000, chains = 4, cores = 4, 
-                      save_pars = save_pars(), file = "./output/models/model_3", file_refit = "on_change",
-                      control = list(adapt_delta = 0.98))
+model3 <- brms::brm(boldness_3 + activity_3 + explore_3 + set_rescor(TRUE), 
+                    data = all_data, iter = 6000, warmup = 2000, chains = 4, cores = 4, 
+                    save_pars = save_pars(), file = "./output/models/model3", file_refit = "on_change",
+                    control = list(adapt_delta = 0.98))
 
-# Look at the MCMC chains.
-  plot(model_3)
+# Compare models  
+model3 <- add_criterion(model3 , c("loo", "waic"), moment_match = TRUE)
 
-# Look at the model
-  summary(model_3)
-
+saveRDS(model3, file = "./output/models/model3.rds")
