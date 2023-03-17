1eaf624eda591091886fb070c05756c7f6325ab6
MaryaneGradito <maryane.gradito@umontreal.ca>
2023-03-17 13:46:52 +1100

    Create model8.R
    created model 8; interaction between bc and parasite load

---
+++ R/model8.R
@@ -0,0 +1,31 @@
+### Script for MODEL 8
+
+#############################
+# Import processed data
+#############################
+dat_E <- read.table("./output/dat_models_E.csv",header=T, sep=",")
+
+#############################
+# Model 8: Parasite load and body condition interaction
+#############################
+
+# Load.packages
+pacman::p_load(lme4, rstan, tidyverse, StanHeaders, jsonlite, rstantools, brms, Rcpp, dplyr, here, flextable, pander)
+
+### Model 8:
+
+boldness_8 <- bf(log_boldness ~ 1 + z_pl + z_bc + z_pl*z-bc + (1 | ID_fish) + (1 | cage)) + gaussian()
+activity_8 <- bf(log_activity ~ 1 + z_pl + z_bc + z_pl*z-bc + (1 | ID_fish) + (1 | cage)) + gaussian()
+explore_8 <- bf(exploration  ~ 1 + z_pl + z_bc + z_pl*z-bc + (1 | ID_fish) + (1 | cage)) + gaussian()
+
+
+model_8 <- brms::brm(boldness_8 + activity_8 + explore_8 + set_rescor(TRUE), 
+                     data = dat_E, iter = 6000, warmup = 2000, chains = 4, cores = 4, 
+                     save_pars = save_pars(), file = "./output/models/model_8", file_refit = "on_change",
+                     control = list(adapt_delta = 0.98))
+
+# Look at the MCMC chains.
+plot(model_8)
+
+# Look at the model
+summary(model_8)
