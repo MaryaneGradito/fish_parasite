0d063fdcaa6ae0a3dd75d2a55c3034c150a85512
MaryaneGradito <maryane.gradito@umontreal.ca>
2023-03-29 17:21:03 +1100

    Update model10.R
    model 10 looking at traits and the individual slopes with the change of bs and bc

--- R/model10.R
+++ R/model10.R
@@ -3,31 +3,36 @@
 #############################
 # Import processed data
 #############################
-dat_parasite <-newdta
+new_dat <- read.table("./output/new_data1.csv",header=T, sep=",")
+
 #############################
-# Model 9: interaction between parasite species
+# Model 10: slopes and change of bs and bc
 #############################
 
 # Load.packages
 pacman::p_load(lme4, rstan, tidyverse, StanHeaders, jsonlite, rstantools, brms, Rcpp, dplyr, here, flextable, pander)
 
-### Model 9: 
+### Model 10: 
 
-boldness_10 <- bf(slope_bold | se(se_bold) ~ delta_pl + delat_bc) + gaussian()
-activity_10 <- bf(slope_activity | se(se_activity) ~ delta_pl + delat_bc) + gaussian()
- explore_10 <- bf(slope_exploration  ~ | se(se_exploration) ~ delta_pl + delat_bc) + gaussian()
+boldness_10 <- bf(bold_E | se(se_b_E) ~ z_change_bs*z_change_bc) + gaussian()
+activity_10 <- bf(act_E | se(se_act_E) ~ z_change_bs*z_change_bc) + gaussian()
+ explore_10 <- bf(exp_E | se(se_exp_E) ~ z_change_bs*z_change_bc) + gaussian()
 
 
 
 model_10 <- brms::brm(boldness_10 + activity_10 + explore_10 + set_rescor(TRUE), 
-                     data = data_new, iter = 6000, warmup = 2000, chains = 4, cores = 4, 
+                     data = new_dat, iter = 6000, warmup = 2000, chains = 4, cores = 4, 
                      save_pars = save_pars(all = TRUE), file = "./output/models/model_10", file_refit = "on_change",
                      control = list(adapt_delta = 0.98))
 
 model_10 <- add_criterion(model_9, c("loo", "waic"), moment_match = TRUE)
 
 # Look at the MCMC chains.
-plot(model_9)
+plot(model_10)
 
 # Look at the model
-summary(model_9)
+summary(model_10)
+
+#change becomes negative
+#when you are below average change in parasite load, activity going up
+#high change in BS, decrease in the animals activity
