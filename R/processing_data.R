0c214ae5a4ebfe4cbef293edb3745d72420f52ab
MaryaneGradito <maryane.gradito@umontreal.ca>
2023-03-29 17:21:19 +1100

    Update processing_data.R
    added some code here to make the other script less intense

--- R/processing_data.R
+++ R/processing_data.R
@@ -275,25 +275,25 @@ all_dat <- read.table("./output/all_data_p.csv",header=T, sep=",")
 all_dat$ces_tot<- (all_dat$P04_alive + all_data$P06)
 
 dat_trial1<-all_dat  %>% 
-  select(ID_fish, trial, cage, treatment, log_boldness, log_activity, exploration, fulton1, parasite_load, BS_post_tot, ces_tot) %>% 
+  select(ID_fish, trial, cage, treatment, log_boldness, log_activity, exploration, fulton1, parasite_load, BS_post_tot, ces_tot, BS_pre) %>% 
   filter(trial == 1) %>% 
   pivot_longer("fulton1",
                values_to='body_condition') %>% arrange(ID_fish)
 
 dat_trial2<-all_dat  %>% 
-  select(ID_fish, trial, cage, treatment, log_boldness, log_activity, exploration,fulton2, parasite_load, BS_post_tot, ces_tot) %>%
+  select(ID_fish, trial, cage, treatment, log_boldness, log_activity, exploration,fulton2, parasite_load, BS_post_tot, ces_tot, BS_pre) %>%
   filter(trial == 2) %>% 
   pivot_longer("fulton2",
                values_to='body_condition') %>% arrange(ID_fish)
 
 dat_trial3<-all_dat  %>% 
-  select(ID_fish, trial, cage, treatment, log_boldness, log_activity, exploration,fulton3, parasite_load, BS_post_tot, ces_tot) %>%
+  select(ID_fish, trial, cage, treatment, log_boldness, log_activity, exploration,fulton3, parasite_load, BS_post_tot, ces_tot, BS_pre) %>%
   filter(trial == 3) %>% 
   pivot_longer("fulton3",
                values_to='body_condition') %>% arrange(ID_fish)
 
 dat_trial4<-all_dat  %>% 
-  select(ID_fish, trial, cage, treatment, log_boldness, log_activity, exploration, fulton4, parasite_load, BS_post_tot, ces_tot) %>%
+  select(ID_fish, trial, cage, treatment, log_boldness, log_activity, exploration, fulton4, parasite_load, BS_post_tot, ces_tot, BS_pre) %>%
   filter(trial == 4) %>% 
   pivot_longer("fulton4",
                values_to='body_condition') %>% arrange(ID_fish)
@@ -305,7 +305,133 @@ dat_trials <- rbind(dat_trial1, dat_trial2, dat_trial3, dat_trial4) %>% arrange(
 dat_par_type <- dat_trials %>% filter(treatment == "E") %>%  mutate(z_bc = scale(body_condition),
                                                              z_pl = scale(parasite_load),
                                                              z_bs = scale(BS_post_tot),
-                                                            z_ces = scale (ces_tot))
+                                                            z_ces = scale (ces_tot),
+                                                            z_bs_pre = scale(BS_pre))
 #Save data
 write.table(dat_par_type, file = "dat_models_parasite.csv",
             sep = ",", row.names = F)
+
+#############################
+# Processing data for model3
+#############################
+#Create data frame with posterior distribution from model3
+#exploration treatment E
+test  <- ranef(model3, summary = FALSE)$ID_fish
+
+exploration_treatmentE_blup <-  test[, , "exploration_treatmentE"]
+posterior <- as_draws_df(model3, vars = "^b")
+exploration_treatmentE_b <- data.frame(posterior[,grep("b_exploration_treatmentE", colnames(posterior))])
+
+fishid_slope <- apply(exploration_treatmentE_blup, 2, function(x) rowSums(exploration_treatmentE_b + x))
+fishid_slope_est <- apply(fishid_slope,2, function(x) mean(x))
+fishid_slope_se <- apply(fishid_slope,2, function(x) sd(x))
+fishid_slope <- cbind(fishid_slope_est,fishid_slope_se)
+
+exp_E<-df(exploration_treatmentE_blup,exploration_treatmentE_b) #function works
+
+#Boldness treatment E
+logboldness_treatmentE_blup <-  test[, , "logboldness_treatmentE"]
+logboldness_treatmentE_b <- data.frame(posterior[,grep("b_logboldness_treatmentE", colnames(posterior))])
+
+bold_E<-df(logboldness_treatmentE_blup,logboldness_treatmentE_b)
+
+#Activity treatmentE
+logactivity_treatmentE_blup <-  test[, , "logactivity_treatmentE"]
+logactivity_treatmentE_b <- data.frame(posterior[,grep("b_logactivity_treatmentE", colnames(posterior))])
+
+act_E<-df(logactivity_treatmentE_blup,logactivity_treatmentE_b)
+
+
+#Bold Intercept
+logboldness_Intercept_blup <-  test[, , "logboldness_Intercept"]
+logboldness_Intercept_b <- data.frame(posterior[,grep("b_logboldness_Intercept", colnames(posterior))])
+
+bold_I<-df(logboldness_Intercept_blup,logboldness_Intercept_b)
+
+#Exploration Intercept
+exploration_Intercept_blup <-  test[, , "exploration_Intercept"]
+exploration_Intercept_b <- data.frame(posterior[,grep("b_exploration_Intercept", colnames(posterior))])
+
+exp_I<-df(exploration_Intercept_blup,exploration_Intercept_b)
+
+#Activity Intercept
+logactivity_Intercept_blup <-  test[, , "logactivity_Intercept"]
+logactivity_Intercept_b <- data.frame(posterior[,grep("b_logactivity_Intercept", colnames(posterior))])
+
+act_I<-df(logactivity_Intercept_blup,logactivity_Intercept_b)
+
+#combine data
+data_slopes<-cbind(bold_E,exp_E,act_E,bold_I,exp_I,act_I)
+dat_slopes<-as.data.frame(data_slopes)
+dat_slopes$ID_fish <- rownames(dat_slopes)
+
+# assigning new names to the columns of the data frame
+colnames(dat_slopes) <- c('bold_E','se_b_E','exp_E','se_exp_E','act_E','se_act_E','bold_I','se_b_I','exp_I','se_exp_I','act_I','se_act_I')
+
+write.table(dat_slopes, file = "dat_slopes.csv",
+            sep = ",", row.names = F)
+
+#############################
+# Processing data for model 10 
+#############################
+#New dataframe for model 10 
+#random slope
+#i need a new data set with the change for parasite load before and after infection,
+# change of bc (first and last, before and after infection)
+#60 fish (only the ones who did the infection)
+
+all_dat <- read.table("./output/all_data_p.csv",header=T, sep=",")
+all_dat$ces_tot<- (all_dat$P04_alive + all_data$P06)
+all_dat$change_bc<- (all_dat$fulton2- all_dat$fulton4)
+all_dat$change_bs<-(all_dat$BS_post_tot - all_dat$BS_pre)
+
+dat_trial1<-all_dat  %>% 
+  select(ID_fish, trial, cage, treatment, log_boldness, log_activity, exploration, fulton1, parasite_load, BS_post_tot, ces_tot, change_bc, change_bs) %>% 
+  filter(trial == 1) %>% 
+  pivot_longer("fulton1",
+               values_to='body_condition') %>% arrange(ID_fish)
+
+dat_trial2<-all_dat  %>% 
+  select(ID_fish, trial, cage, treatment, log_boldness, log_activity, exploration,fulton2, parasite_load, BS_post_tot, ces_tot, change_bc, change_bs) %>%
+  filter(trial == 2) %>% 
+  pivot_longer("fulton2",
+               values_to='body_condition') %>% arrange(ID_fish)
+
+dat_trial3<-all_dat  %>% 
+  select(ID_fish, trial, cage, treatment, log_boldness, log_activity, exploration,fulton3, parasite_load, BS_post_tot, ces_tot, change_bc, change_bs) %>%
+  filter(trial == 3) %>% 
+  pivot_longer("fulton3",
+               values_to='body_condition') %>% arrange(ID_fish)
+
+dat_trial4<-all_dat  %>% 
+  select(ID_fish, trial, cage, treatment, log_boldness, log_activity, exploration, fulton4, parasite_load, BS_post_tot, ces_tot, change_bc, change_bs) %>%
+  filter(trial == 4) %>% 
+  pivot_longer("fulton4",
+               values_to='body_condition') %>% arrange(ID_fish)
+
+#rbind together to get dataset for the model
+dat_trials <- rbind(dat_trial1, dat_trial2, dat_trial3, dat_trial4) %>% arrange(ID_fish)
+
+#Select for experimentally infected fish and scale parasite and body condition
+dat_par_type <- dat_trials %>% filter(treatment == "E" & trial == 4) %>%  mutate(z_bc = scale(body_condition),
+                                                                                 z_pl = scale(parasite_load),
+                                                                                 z_bs = scale(BS_post_tot),
+                                                                                 z_ces = scale (ces_tot),
+                                                                                 z_change_bc = scale(change_bc), z_change_bs = scale(change_bs))
+
+#Save data
+write.table(dat_par_type, file = "new_data.csv",
+            sep = ",", row.names = F)
+
+dat_change <- read.table("./output/new_data.csv",header=T, sep=",")
+
+#merge slopes and change in bs and bc
+new_data<-merge(dat_slopes, dat_change, by = "ID_fish")
+
+write.table(new_data, file = "new_data1.csv",
+            sep = ",", row.names = F)
+
+
+
+
+
