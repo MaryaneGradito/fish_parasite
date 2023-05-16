0c9bf82f60c33ec734a9c818bdb81dc83fd6a31e
MaryaneGradito <maryane.gradito@umontreal.ca>
2023-05-16 19:03:41 -0400

    Update cromwell_map.R
    worked on cromwell map

--- cromwell_map.R
+++ cromwell_map.R
@@ -2,26 +2,31 @@
 #Script de Juliane Vigneault
 
 # ----- Loading packages ----- #
-install.packages("sf")
 library(sf)
 library(dplyr)
-install.packages("measurements")
 library(measurements)
 library(ggplot2)
 library(stringr)
+library(tmap)    # for static and interactive maps
+library(leaflet) # for interactive maps
+library(ggplot2) # tidyverse data visualization package
+library(ggspatial)
+library(magick)
 
 # ----- Loading data ----- #
 
-CombinedData <- read.csv("./map/cromwell.csv",header=T, sep=";") # ---- Étape 1) Load ton fichier de données avec tes coordonnées GPS
+CombinedData <- read.csv("./map/cromwell.csv",header=T, sep=";",stringsAsFactors=FALSE, fileEncoding="latin1")
+
+ # ---- Étape 1) Load ton fichier de données avec tes coordonnées GPS
 
 ## Attribute table ##
 # ---- Étape 2) Sélection les variables d'intérêt dans ton fichier
 attributes <- CombinedData %>% 
-  select_("cage", "lat", "long")
+  select_("cage", "lat", "long", "dens_tot","dens_ces","dens_bs")
 
 # Coordinates conversion #
 # ---- Étape 3) Les coordonnées que tu veux représenter doivent être en format degré décimale, alors ça se peut que tu ailles à convertir tes données de terrain
-attributes$lat <- str_replace(attributes$lat, "�", " ")
+attributes$lat <- str_replace(attributes$lat, "°", " ")
 attributes$lat <- str_remove(attributes$lat, "'")
 attributes$lat <- conv_unit(attributes$lat, from = "deg_dec_min", to = "dec_deg")
 attributes$lat <- as.numeric(attributes$lat)
@@ -39,9 +44,50 @@ CROM <- st_read("./map/Cromwell.shp")
 
 cage_number<-as.factor(attributes$cage)
 # ---- Étape 5) Représentation visuelle
+# Specify the number of decimal places in the labels
+
+#map for black spot density
 CROM.plot <- ggplot() + 
-  geom_sf(data = CROM, fill = "lightblue") + #Shape du lac
-  geom_point(data = attributes, aes(x = long, y = lat, color = cage_number), size = 3.5) #Points GPS de tes cages
-  #geom_text(data = attributes, aes(x = long, y = lat), size = 2) + #Si tu veux ajouter des étiquettes à tes points
-  theme_void() #Pour mettre sur fond blanc plutôt qu'un graphique
-CROM.plot
+  geom_sf(data = CROM, fill = "#DFEFFE", color = "black")+
+scale_x_continuous(breaks=c(-74.002,-73.998, -73.994),labels = function(x) paste0(format(x, digit = 5), "\u00B0","W")) +
+scale_y_continuous(breaks=c(45.988,45.989, 45.990),labels = function(y) paste0(format(y, digit = 5), "\u00B0","N")) +
+  geom_point(data = attributes, aes(x = long, y = lat, color = dens_bs), size = 9)+ #Points GPS de tes cages
+  geom_text(data = attributes, aes(x = long, y = lat, label = cage), size = 7) +
+  theme_classic() +
+  labs(title = "", 
+       subtitle = "") +
+  xlab("") + ylab("")
+CROM.plot<-CROM.plot + theme(plot.title = element_text(size=14, face="bold")) + theme(panel.background = element_rect(fill = '#F7F7F7', color = 'black')) + scale_colour_gradient(low = "#FEFFD0",high = "#B80A0A")
+
+p1<-CROM.plot +annotation_scale(location = "bl", bar_cols = c("black","white")) +
+  annotation_north_arrow(location = "tl", which_north = "true",
+                         style = north_arrow_nautical(fill =c("black","white"),line_col = "black"))+ theme(legend.position = c(0.88, 0.40), legend.background = element_rect(fill = "#F7F7F7", color = "black"), legend.key.size = unit(0.8, 'cm'), legend.title = element_text(size=15,face = "bold"), legend.text = element_text(size=12),plot.margin=unit(c(1,1,-0.5,1), "cm")) +
+  labs(color = "Mean \n black spot \n density (no/g) \n per cage")
+  
+
+#map for cestode density
+CROM.plot <- ggplot() + 
+  geom_sf(data = CROM, fill = "#DFEFFE", color = "black")+
+  scale_x_continuous(breaks=c(-74.002,-73.998, -73.994),labels = function(x) paste0(format(x, digit = 5), "\u00B0","W")) +
+  scale_y_continuous(breaks=c(45.988,45.989, 45.990),labels = function(y) paste0(format(y, digit = 5), "\u00B0","N")) +
+  geom_point(data = attributes, aes(x = long, y = lat, color = dens_ces), size = 9)+ #Points GPS de tes cages
+  geom_text(data = attributes, aes(x = long, y = lat, label = cage), size = 7) +
+  theme_classic() +
+  labs(title = "", 
+       subtitle = "") +
+  xlab("") + ylab("")
+
+CROM.plot<-CROM.plot + theme(plot.title = element_text(size=14, face="bold")) + theme(panel.background = element_rect(fill = '#F7F7F7', color = 'black')) + scale_colour_gradient(low = "#FEFFD0",high = "#B80A0A")
+
+p2<-CROM.plot +annotation_scale(location = "bl", bar_cols = c("black","white")) +
+  annotation_north_arrow(location = "tl", which_north = "true",
+                         style = north_arrow_nautical(fill =c("black","white"),line_col = "black")) + theme(legend.position = c(0.88, 0.40), legend.background = element_rect(fill = "#F7F7F7", color = "black"), legend.key.size = unit(0.8, 'cm'), legend.title = element_text(size=15, face = "bold"), legend.text = element_text(size=12), plot.margin=unit(c(-0.5,1,1,1), "cm")) + labs(color = "Mean \n cestode \n density (no/g) \n per cage")
+
+
+p<-grid.arrange(p1,p2)
+
+
+
+ggsave("./output/figures/map.png", plot = p, width = 40, height = 30, units = "cm")
+
+map <- image_read("./output/figures/map.png")
