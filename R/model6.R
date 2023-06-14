4ccacf101125c2760d20e0ba59b369f7b7c1c50f
MaryaneGradito <maryane.gradito@umontreal.ca>
2023-06-14 17:04:10 -0400

    Update model6.R
    changed dens tot so rerun the model

--- R/model6.R
+++ R/model6.R
@@ -19,9 +19,9 @@
   explore_6 <- bf(z_exploration  ~ 1 + z_dens + z_bc + (1 | ID_fish) + (1 | cage)) + gaussian()
 
   
-  model_6 <- brms::brm(boldness_6 + activity_6 + explore_6 + set_rescor(TRUE), 
+  model_6_1 <- brms::brm(boldness_6 + activity_6 + explore_6 + set_rescor(TRUE), 
                        data = dat_E, iter = 6000, warmup = 2000, chains = 4, cores = 4, 
-                       save_pars = save_pars(), file = "./output/models/model_6", file_refit = "on_change",
+                       save_pars = save_pars(), file = "./output/models/model_6_1", file_refit = "on_change",
                        control = list(adapt_delta = 0.98))
 
 # Look at the MCMC chains.
